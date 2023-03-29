c1344be3bdf3ec4c870b441907af83c0f277115a
Zahra Mirzaei <z.mirzaei2019@gmail.com>
2023-03-29 22:27:59 +0330

    feat: get object keys as union type

--- src/01-inference-basics/04-get-object-keys.problem.ts
+++ src/01-inference-basics/04-get-object-keys.problem.ts
@@ -12,6 +12,18 @@ const testingFrameworks = {
   },
 };
 
-type TestingFramework = unknown;
+/**
+ * you don't need to use it with typeof, you can use it with any type like:
+ * type TestingFrameworks={
+ *  a: string;
+ *  b: string;
+ *  c: number;
+ * };
+ * type TestingFramework= keyof TestingFrameworks;
+ *
+ * but when it come with typeof, we don't need to declare an extra type.
+ */
+
+type TestingFramework = keyof typeof testingFrameworks;
 
 type tests = [Expect<Equal<TestingFramework, "vitest" | "jest" | "mocha">>];
